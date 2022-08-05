17ea4f64254b2de19e95f105bdb368fc22cc2da6
Muhammed-Haci <muhammed.hacilar@gmail.com>
2022-08-05 19:10:34 +0300

    u

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
+// ============= component Functions ============= //
+
+// === Add active class ===: 
+function removeActiveClass(ele) {
+  ele.forEach((el) => el.classList.remove("active"));
+}
+
+// === remove active class ===: 
+function addActiveClass(ele) {
+  ele.forEach((el) => el.classList.add("active"));
+}
+
 // =========== create overlay function =========== //
 function createOverlay() {
   const overlay = document.createElement("div");
@@ -47,12 +59,11 @@ let toggleBtn = document.querySelector(".toggle__manu");
 
 toggleBtn.addEventListener("click", () => toggleBtn.classList.toggle("open"));
 
-document.addEventListener("click", (e) => {
-  if (toggleBtn.classList.contains("open") && !e.target.classList.contains(".toggle__manu")) {
-      // toggleBtn.classList.remove("open");
-      console.log("Hello");
-  }
-});
+// document.addEventListener("click", (e) => {
+  // if (toggleBtn.classList.contains("open") && !e.currentTarget.classList.contains(".toggle__manu")) {
+  //   toggleBtn.classList.remove("open");
+  // }
+// });
 
 // ===== toggle open class to lang Menu ======== //
 
@@ -62,8 +73,30 @@ langMenu.addEventListener("click", () => langMenu.classList.toggle("open"));
 
 // document.addEventListener("click", (e) => {
 //   if (langMenu.classList.contains("open") && !e.target.classList.contains(".toggle__manu")) {
-//       // toggleBtn.classList.remove("open");
+      // toggleBtn.classList.remove("open");
 //       console.log("Hello");
 //   }
 // });
+
+
+// ===== toggle between the about filter ======= //
+
+let buttons = document.querySelectorAll("#about .filter li");
+let divs = document.querySelectorAll("#about .info div");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    buttons.forEach((button) => button.classList.remove("active"));
+    e.target.classList.add("active");
+
+    divs.forEach((div) => {
+      div.classList.remove("active");
+      if (div.dataset.text === e.target.dataset.text) {
+        div.classList.add("active");
+      }
+    });
+
+
+  });
+})
 //# sourceMappingURL=main.js.map
